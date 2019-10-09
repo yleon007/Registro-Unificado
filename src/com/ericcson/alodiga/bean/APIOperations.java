eca0054b4f48ee10e2a4cd63b00c2c4fbdb425e1
moisegrat1212 <moisegrat12@gmail.com>
2019-10-09 10:38:38 -0400

    s

--- src/com/ericcson/alodiga/bean/APIOperations.java
+++ src/com/ericcson/alodiga/bean/APIOperations.java
@@ -117,6 +117,8 @@ public class APIOperations {
     
     private static final Logger logger = Logger.getLogger(APIOperations.class);
     
+    
+    //PRUEBA
     private boolean validarUsuario(String usuario, String password) {
         UsuarioWS usuarioWS = entityManager
                 .createNamedQuery("UsuarioWS.findUserAndPassword",
